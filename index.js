89f180453fb580d9a9cdc8ad0faffe0eef739b10
Lovedeep Singh <748774@student.tor.loc>
2019-06-19 14:54:36 -0400

    thirddd commit

--- index.js
+++ index.js
@@ -9,4 +9,25 @@ draw:function(){
 }
 };
 
+function createCircle(radius){
+    return{
+    
+        radius,
+     
+        draw:function(){
+            console.log('draw');
+        }
+        };
+}
+const circle = createCircle(1);
 circle.draw();
+
+//cosntructor function
+
+function Circle(radius){
+    this.radius=radius;
+    this.draw= function(){
+        console.log("draw");
+    }
+}
+const another =new Circle(1);
